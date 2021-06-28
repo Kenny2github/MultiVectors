87318c0e6526feee6ead7536eccc6b398668fa43
Kenny2github <kenny2minecraft@gmail.com>
2021-06-29 05:10:05 +0800

    Set up for setup.py (upcoming) to read

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# MultiVectors
-A Python library that handles multivectors in n-dimensional space.
+# multivectors
+Compute blades and multivectors in arbitrary-dimensional space.
+
+## Installation
+```
+pip install multivectors
+```
+
+## Usage
+```py
+import math
+from multivectors import x, y, z
+
+v = 2*x + 3*y + 4*z
+print(v.rotate(math.pi/2, x * y))
+```
+Output:
+```
+(-3.00x + 2.00y + 4.00z)
+```
--- multivectors.py
+++ multivectors.py
@@ -1,8 +1,42 @@
+"""
+# multivectors
+Compute blades and multivectors in arbitrary-dimensional space.
+
+## Installation
+```
+pip install multivectors
+```
+
+## Usage
+```py
+import math
+from multivectors import x, y, z
+
+v = 2*x + 3*y + 4*z
+print(v.rotate(math.pi/2, x * y))
+```
+Output:
+```
+(-3.00x + 2.00y + 4.00z)
+```
+"""
 from __future__ import annotations
 from itertools import combinations
 import math
 from numbers import Real
 from typing import Dict, List, Tuple, Union
+
+__all__ = [
+    'Blade',
+    'MultiVector',
+    '_',
+    'x',
+    'y',
+    'z',
+    'w'
+]
+
+__version__ = '0.0.0'
 
 NAMES = 'xyzw'
 
