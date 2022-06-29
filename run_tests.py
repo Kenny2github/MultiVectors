8956a7dc4230ebcc0d4d03599985c7c0e46650bb
AbyxDev <ken@abyx.dev>
2022-06-29 00:30:12 -0700

    Move setup and tests to pyproject.toml and pytest

--- run_tests.py
+++
@@ -1,9 +0,0 @@
-import sys
-import doctest
-import multivectors
-
-fails = doctest.testmod(multivectors)[0]
-fails += doctest.testfile('README.md')[0]
-fails += doctest.testfile('docs.md')[0]
-if fails > 0:
-    sys.exit(1)
